1dde19e0af43d46aae065dbad3e35b10fdd70df6
BruceWang1030 <r395wang@uwaterloo.ca>
2019-06-17 20:03:03 -0400

    price-guess

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import house from "./img/sample.jpg";
+import "./App.css";
+
+class Score extends React.Component {
+  render() {
+    return <h1 className="score">Score: {this.props.score}</h1>;
+  }
+}
+
+class HousePicture extends React.Component {
+  render() {
+    function myfunction() {
+      console.log("Clicked");
+    }
+    return (
+      <button className="btn-img">
+        <div>
+          <img
+            src={house}
+            alt="my house"
+            onClick={myfunction}
+            className="house-img"
+          />
+        </div>
+      </button>
+    );
+  }
+}
+
+class Buttons extends React.Component {
+  render() {
+    function submit() {
+      console.log("submitted");
+    }
+    function skip() {
+      console.log("skipped");
+    }
+    function hint() {
+      console.log("Hint shown");
+    }
+    return (
+      <div className="btn-bottom">
+        <button onClick={skip} className="btn-skip">
+          Skip
+        </button>
+        <button onClick={submit} className="btn-submit">
+          Submit
+        </button>
+        <button onClick={hint} className="btn-hint">
+          Hint
+        </button>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Score score="64" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HousePicture />
       </header>
+      <div>
+        <Buttons />
+      </div>
     </div>
   );
 }
