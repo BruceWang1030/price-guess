e7817709357784eb1205ba0f011c2ab22faf138b
BruceWang1030 <r395wang@uwaterloo.ca>
2019-06-18 17:02:05 -0400

    first draft flip and slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import house from "./img/sample.jpg";
-import Slider from "react-toolbox/lib/slider";
 import "./App.css";
 
 class Score extends React.Component {
@@ -25,6 +24,36 @@ class HousePicture extends React.Component {
           />
         </div>
       </button>
+    );
+  }
+}
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { flipped: false, clicked: false };
+    this.flip = this.flip.bind(this);
+  }
+  flip() {
+    console.log("flip clicked");
+    this.setState({
+      flipped: !this.state.flipped,
+      clicked: true
+    });
+  }
+  render() {
+    var flippedCSS = this.state.flipped
+      ? " Card-Back-Flip"
+      : " Card-Front-Flip";
+    if (!this.state.clicked) flippedCSS = "";
+    return (
+      <div className="Card" onClick={this.flip}>
+        <div className={"Card-Front" + flippedCSS}>
+          <h3>
+            <img src={house} alt="my house" className="house-img" />
+          </h3>
+        </div>
+        <div className={"Card-Back" + flippedCSS}>aaaaa</div>
+      </div>
     );
   }
 }
@@ -58,43 +87,58 @@ class Buttons extends React.Component {
 
 //Slider Part
 class PriceRange extends React.Component {
-  state = {
-    slider1: 5
-  };
-  handleChange = (slider, value) => {
-    const newState = {};
-    newState[slider] = value;
-    this.setState(newState);
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.value,
+      min: this.props.min,
+      max: this.props.max
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
     return (
-      <section>
-        <p>Normal slider</p>
-        <Slider
-          min={1}
-          max={100}
-          value={this.state.slider1}
-          onChange={this.handleChange.bind(this, "slider1")}
-        />
-      </section>
+      <div>
+        <label>
+          <input
+            id="typeinp"
+            type="range"
+            min={this.state.min}
+            max={this.state.max}
+            value={this.state.value}
+            onChange={this.handleChange}
+            step="10000"
+            className="price-range"
+          />
+        </label>
+        <div>${this.state.value}</div>
+      </div>
     );
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Score score="64" />
-      <header className="App-header">
-        <HousePicture />
-      </header>
-      <div>
-        <PriceRange />
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Score score="64" />
+        <header className="App-header">
+          <Card />
+        </header>
+        <div>
+          <PriceRange value={300000} min={100000} max={500000} />
+        </div>
+        <div>
+          <Buttons />
+        </div>
       </div>
-      <div>
-        <Buttons />
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
 export default App;
